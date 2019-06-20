355c25eaa660e975d437a0c6dab5a083f176f3eb
myellen <max.yellen@gmail.com>
2019-06-20 14:25:57 -0400

    custom installer to test changes

--- extras/installer.sh
+++ extras/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ORIGIN_REPO="https://github.com/${GIT_OWNER:-mrworf}/plexupdate"
+ORIGIN_REPO="https://github.com/${GIT_OWNER:-myellen}/plexupdate"
 FULL_PATH="/opt/plexupdate"
 CONFIGFILE="/etc/plexupdate.conf"
 CONFIGCRON="/etc/plexupdate.cron.conf"
